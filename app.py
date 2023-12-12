582cb79323071def5964cb0c9c7cdc3923b6d862
Richard Nwokoye <RNwokoye@gmail.com>
2023-12-12 01:33:21 -0500

    req file

--- app.py
+++ app.py
@@ -10,42 +10,44 @@ import os
 
 st.title("Hello Streamlit")
 
-
-def download_to_temp_file(bucket_name, s3_file_key):
-    s3 = boto3.client("s3")
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-
-    s3.download_fileobj(bucket_name, s3_file_key, temp_file)
-    temp_file.close()  # Close the file so it can be reopened later by other processes
-
-    return temp_file.name
+st.subheader("Test Boto3")
 
 
-temp_cert_file_path3 = download_to_temp_file(
-    st.secrets.CA_CERT.bucket, st.secrets.CA_CERT.file
-)
+# def download_to_temp_file(bucket_name, s3_file_key):
+#     s3 = boto3.client("s3")
+#     temp_file = tempfile.NamedTemporaryFile(delete=False)
 
-st.write(temp_cert_file_path3)
+#     s3.download_fileobj(bucket_name, s3_file_key, temp_file)
+#     temp_file.close()  # Close the file so it can be reopened later by other processes
 
-conn9 = psycopg2.connect(
-    dbname=st.secrets.connections.dbname,
-    user=st.secrets.connections.user,
-    password=st.secrets.connections.password,
-    host=st.secrets.connections.host,
-    sslmode="require",
-    port=st.secrets.connections.port,
-    sslrootcert=temp_cert_file_path3,  # Use the temp file path
-)
+#     return temp_file.name
 
-q9 = "SELECT * FROM traffic_tickets;"
 
-with conn9.cursor() as cur:
-    cur.execute(q9)
-    res9 = cur.fetchall()
-    data9 = pd.DataFrame(res9)
+# temp_cert_file_path3 = download_to_temp_file(
+#     st.secrets.CA_CERT.bucket, st.secrets.CA_CERT.file
+# )
 
-st.write(data9)
+# st.write(temp_cert_file_path3)
 
-os.remove(temp_cert_file_path3)
+# conn9 = psycopg2.connect(
+#     dbname=st.secrets.connections.dbname,
+#     user=st.secrets.connections.user,
+#     password=st.secrets.connections.password,
+#     host=st.secrets.connections.host,
+#     sslmode="require",
+#     port=st.secrets.connections.port,
+#     sslrootcert=temp_cert_file_path3,  # Use the temp file path
+# )
 
-conn9.close()
+# q9 = "SELECT * FROM traffic_tickets;"
+
+# with conn9.cursor() as cur:
+#     cur.execute(q9)
+#     res9 = cur.fetchall()
+#     data9 = pd.DataFrame(res9)
+
+# st.write(data9)
+
+# os.remove(temp_cert_file_path3)
+
+# conn9.close()
