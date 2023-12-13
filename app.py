7611ad7eeec5b26cd0e9f360d91faaead5f50026
Richard Nwokoye <RNwokoye@gmail.com>
2023-12-13 14:37:52 -0500

    debugging

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ st.subheader("Test Boto3")
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query("SELECT * FROM mytable;", ttl="10m")
+df = conn.query("SELECT * FROM public.mytable;", ttl="10m")
 
 # Print results.
 for row in df.itertuples():
